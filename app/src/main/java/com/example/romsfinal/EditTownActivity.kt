57dd1415df95c030c4ddda2819d0eae5cbcd5595
emilyhu8 <91348233+emilyhu8@users.noreply.github.com>
2022-05-16 21:30:40 -0400

    added new towns

--- app/src/main/java/com/example/romsfinal/EditTownActivity.kt
+++ app/src/main/java/com/example/romsfinal/EditTownActivity.kt
@@ -13,7 +13,7 @@ class EditTownActivity : BaseActivity() {
     private lateinit var topAttraction: TextView
     private lateinit var category: TextView
     private lateinit var photo:ImageView
-    var photos= mutableListOf<Int>(R.drawable.subicbay, R.drawable.church, R.drawable.heritage)
+    var photos= mutableListOf(R.drawable.subicbay, R.drawable.church, R.drawable.heritage,R.drawable.island, R.drawable.city)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_town)
@@ -23,7 +23,7 @@ class EditTownActivity : BaseActivity() {
         category=findViewById(R.id.category)
         photo=findViewById(R.id.photo)
 
-        val position = intent.extras?.getInt("name")
+        val position = intent.extras!!.getInt("position")
         val inputName = intent.getStringExtra("name").toString()
         val inputTopAttraction = intent.getStringExtra("topAttraction").toString()
         val inputTopFood = intent.getStringExtra("topFood").toString()
@@ -33,7 +33,7 @@ class EditTownActivity : BaseActivity() {
         topAttraction.text = "Top atttraction: "+inputTopAttraction
         topFood.text = "Top Food: "+inputTopFood
         category.text = inputCategory
-        photo.setImageResource(photos[position!!])
+        photo.setImageResource(photos[position%photos.size])
 
 
     }
--- app/src/main/java/com/example/romsfinal/Repository.kt
+++ app/src/main/java/com/example/romsfinal/Repository.kt
@@ -2,10 +2,10 @@ package com.example.romsfinal
 
 class Repository private constructor(){
     companion object{
-         var names=listOf("Subic Bay", "Cavite", "Silay City")
-         var topAttraction=listOf("Pamulaklakin Forest Trail", "San Roque Parish Church", "Hofilena Heritage House")
-        var topFood=listOf("Xtremely Xpresso Cafe", "Vakante Food Hub", "PUNONG Gary's Place")
-         var category=listOf("Children Friendly!", "Historical", "Historical")
+         var names=listOf("Subic Bay", "Cavite", "Silay City", "Siquijor", "Baguio City")
+         var topAttraction=listOf("Pamulaklakin Forest Trail", "San Roque Parish Church", "Hofilena Heritage House", "Siquijor Butterfly Sanctuary", "Burnham Park")
+        var topFood=listOf("Xtremely Xpresso Cafe", "Vakante Food Hub", "PUNONG Gary's Place", "D'Barkads Food Haus", "Chef's Home Baguio City")
+         var category=listOf("Children Friendly!", "Historical", "Historical", "Children Friendly", "Children Friendly")
         var places = mutableListOf<Towns>()
 
     }
